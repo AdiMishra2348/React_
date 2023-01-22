77aa39f7447ad03ab6979868a608333dec96bce3
Aditya Mishra <108024126+AdiMishra2348@users.noreply.github.com>
2023-01-22 18:58:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Components/Footer";
+import Heading from "./Components/Heading";
+
+var React = require("react");
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
